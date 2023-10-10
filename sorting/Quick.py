bdb00fa9837f29823e3cd62fc5a2bbabdebe3cf3
madsbastholm <madsbrriinckbastholm@gmail.com>
2023-10-10 13:01:16 +0200

    did some stuff

--- sorting/Quick.py
+++ sorting/Quick.py
@@ -6,7 +6,7 @@ for i in range(0, len(array)):
     key = array[i]
     j = i - 1
     while j >= 0 and array[i] < array[j]:
-        array[i] = array[j + 1]
+        array[i] = array[j + 1] 
         j -= 1
     array[j] = key
 
