be8b90d74dc7c94b7420c7b266534e5b3499eeb4
Mana42138 <86923626+Mana42138@users.noreply.github.com>
2023-10-10 12:49:05 +0200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+from sorting.Bubble import Bubb
+from sorting.Insertion import Ins
+from sorting.Merge import merge_sort
+def Bubble(array):
+    return Bubb(array)
+
+def Insert(array):
+    return Ins(array)
+
+def Merge(array):
+    return merge_sort(array)
