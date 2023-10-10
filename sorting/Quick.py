e070d6ee9c4054032a2dbaaa0e37d3587b7f1862
madsbastholm <madsbrriinckbastholm@gmail.com>
2023-10-10 12:58:42 +0200

    nothing

--- sorting/Quick.py
+++ sorting/Quick.py
@@ -2,4 +2,12 @@
 
 array = [2, 1, -3, 5]
 
-for i in 
+for i in range(0, len(array)):
+    key = array[i]
+    j = i - 1
+    while j >= 0 and array[i] < array[j]:
+        array[i] = array[j + 1]
+        j -= 1
+    array[j] = key
+
+print(array)
