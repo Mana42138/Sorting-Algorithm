f8c78ecc4806935cfd910fb5b34ce47df38317f4
madsbastholm <madsbrriinckbastholm@gmail.com>
2023-10-13 14:21:43 +0200

    simplified

--- list.py
+++ list.py
@@ -5,9 +5,7 @@ from sorting.Insertion import *
 from sorting.Merge import *
 from sorting.Quick import *
 from sorting.Py_sort import *
-from filesys.functions.read import *
-from filesys.functions.write import *
-from filesys.functions.delete import *
+from filesys.handler import *
 from dotenv import *
 
 import sys
