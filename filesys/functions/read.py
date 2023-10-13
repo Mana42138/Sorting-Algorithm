47539895539355eaa051b0c37855e51b49d373fb
madsbastholm <madsbrriinckbastholm@gmail.com>
2023-10-13 14:33:44 +0200

    dsada

--- filesys/functions/read.py
+++ filesys/functions/read.py
@@ -2,7 +2,7 @@ from devs_defaults_v1 import *
 
 def readfile(data_file : str):
     """
-        data_file: The file path you want to read.
+    data_file: The file path you want to read.
     """
     with open(data_file, "r") as f:
         data = jn.load(f)
